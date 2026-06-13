581fe5e989d8b56ca628284aecf1ab8366bf46f3
KimPotatoIV <jossceo@gmail.com>
2026-06-13 13:07:29 +0900

    Reorder and fix projectile collision setup
    Move collision layer/mask configuration to the top of _ready so it's applied before waiting for the physics frame, remove the duplicated setup, enable the contact monitor (updated comment), and correct a comment typo for the body_entered signal connection.

--- scenes/projectile/projectile.gd
+++ scenes/projectile/projectile.gd
@@ -2,10 +2,18 @@ extends RigidBody2D
 
 ##################################################
 func _ready() -> void:
+	# 내가 누구인가?
+	set_collision_layer_value(1, false)	# 1번 레이어에서 나를 제외
+	set_collision_layer_value(2, true)	# 나를 2번 레이어로 지정
+	
+	# 누구랑 부딪힐 것인가?
+	set_collision_mask_value(1, true)	# 1번 레이어와는 닿았을 때 충돌을 감지
+	set_collision_mask_value(2, false)	# 2번 레이어끼리는 부딪히지 않고 통과
+	
 	# 물리 엔진이 완벽하게 준비된 뒤에 설정을 적용
 	await get_tree().physics_frame
 	
-	# 처음에는 충돌 감지 모니터를 꺼둠
+	# 충돌 감지 모니터를 켬
 	contact_monitor = true
 	# 동시에 감지할 수 있는 최대 충돌 횟수를 1개로 제한
 	max_contacts_reported = 1
@@ -15,15 +23,7 @@ func _ready() -> void:
 	# 공기저항을 0.0으로 만들어, 날아가는 동안 속도가 수평 방향으로 줄어들지 않고 일정하게 만듦
 	linear_damp = 0.0
 	
-	# 내가 누구인가?
-	set_collision_layer_value(1, false)	# 1번 레이어에서 나를 제외
-	set_collision_layer_value(2, true)	# 나를 2번 레이어로 지정
-	
-	# 누구랑 부딪힐 것인가?
-	set_collision_mask_value(1, true)	# 1번 레이어와는 닿았을 때 충돌을 감지
-	set_collision_mask_value(2, false)	# 2번 레이어끼리는 부딪히지 않고 통과
-	
-	# 다른 물리 바디와 충동 시, body_entered 시그널을 _on_body_entered() 함수와 연결
+	# 다른 물리 바디와 충돌 시, body_entered 시그널을 _on_body_entered() 함수와 연결
 	body_entered.connect(_on_body_entered)
 
 ##################################################
